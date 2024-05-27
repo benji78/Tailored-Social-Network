f20ece41e32cade8f422cf6e025b9e4ec59167e0
Finalblue <coco.michoumineur@gmail.com>
2024-05-27 23:56:54 +0200

    feat : tags are now part of the algorithm for recommandation + graph visualisation

--- src/components/FriendRecommendations/FriendRecommendations.tsx
+++ src/components/FriendRecommendations/FriendRecommendations.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react'
 import supabase from '@/lib/supabase'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import { useAuth } from '../auth-context'
 
 interface UserData {
   connections: string[]
-  interests: string[]
+  projectTags: string[]
+  connectedUsersTags: string[]
 }
 
 interface Recommendation {
@@ -23,49 +24,87 @@ const FriendRecommendations: React.FC = () => {
 
   useEffect(() => {
     const fetchUserData = async (userId: string): Promise<UserData> => {
-      const { data: connectionsData } = await supabase
-        .from<string, any>('connections')
-        .select('friend_id')
-        .eq('user_id', userId)
+      const { data: connectionsData } = await supabase.from('connections').select('friend_id').eq('user_id', userId)
 
-      const { data: interestsData } = await supabase
-        .from<string, any>('is_interested_in')
-        .select('interest_id')
-        .eq('user_id', userId)
+      const { data: projectData } = await supabase.from('projects').select('id').eq('user_id', userId)
+
+      const projectIds = projectData ? projectData.map((project: { id: any }) => project.id) : []
+
+      const { data: tagsData } = await supabase.from('have_tags').select('tags_id').in('project_id', projectIds)
 
       const connections = connectionsData ? connectionsData.map((conn: { friend_id: any }) => conn.friend_id) : []
-      const interests = interestsData ? interestsData.map((interest: { interest_id: any }) => interest.interest_id) : []
+      const projectTags = tagsData ? tagsData.map((tag: { tags_id: any }) => tag.tags_id) : []
 
-      return { connections, interests }
+      // Fetch tags of connected users
+      const connectedUsersTags: string[] = []
+      for (const connectionId of connections) {
+        const { data: connectedProjectData } = await supabase.from('projects').select('id').eq('user_id', connectionId)
+        const connectedProjectIds = connectedProjectData
+          ? connectedProjectData.map((project: { id: any }) => project.id)
+          : []
+        const { data: connectedTagsData } = await supabase
+          .from('have_tags')
+          .select('tags_id')
+          .in('project_id', connectedProjectIds)
+        const connectedTags = connectedTagsData ? connectedTagsData.map((tag: { tags_id: any }) => tag.tags_id) : []
+        connectedUsersTags.push(...connectedTags)
+      }
+
+      return { connections, projectTags, connectedUsersTags }
     }
 
     const calculateCommonConnections = (userConnections: string[], otherConnections: string[]): number => {
-      return userConnections.filter((conn) => otherConnections.includes(conn)).length
+      return userConnections.filter((conn) => otherConnections.includes(conn)).length // Return number of connection in common
     }
 
-    const calculateCommonInterests = (userInterests: string[], otherInterests: string[]): number => {
-      return userInterests.filter((interest) => otherInterests.includes(interest)).length
+    const calculateCommonProjectTags = (userTags: string[], otherTags: string[]): number => {
+      return userTags.filter((tag) => otherTags.includes(tag)).length // Return number of tags in common
     }
 
     const calculateRecommendationScore = (
       userConnections: string[],
-      userInterests: string[],
+      userProjectTags: string[],
+      userConnectedTags: string[],
       otherConnections: string[],
-      otherInterests: string[]
+      otherProjectTags: string[],
+      otherConnectedTags: string[]
     ): number => {
       const commonConnections = calculateCommonConnections(userConnections, otherConnections)
-      const commonInterests = calculateCommonInterests(userInterests, otherInterests)
-      const connectionWeight = 0.7
-      const interestWeight = 0.3
-      return connectionWeight * commonConnections + interestWeight * commonInterests
+      const commonProjectTags = calculateCommonProjectTags(userProjectTags, otherProjectTags) // Return number of tags in common
+      const commonConnectedTags = calculateCommonProjectTags(userConnectedTags, otherConnectedTags) // Return number of direct friends tags in common with users
+
+      const connectionWeight = 0.5
+      const tagWeight = 0.3
+      const connectedTagWeight = 0.2
+
+      console.log({
+        connectionWeight,
+        commonConnections,
+        tagWeight,
+        commonProjectTags,
+        connectedTagWeight,
+        commonConnectedTags,
+      })
+
+      return (
+        connectionWeight * commonConnections + tagWeight * commonProjectTags + connectedTagWeight * commonConnectedTags
+      )
     }
 
     const generateFriendRecommendations = async () => {
       const currentUser = session?.user?.id
       if (currentUser) {
-        const { connections: userConnections, interests: userInterests } = await fetchUserData(currentUser)
-        console.log({ connections: userConnections, interests: userInterests })
-        const { data: allUsers } = await supabase.from<string, any>('users2').select('*')
+        const {
+          connections: userConnections,
+          projectTags: userProjectTags,
+          connectedUsersTags: userConnectedTags,
+        } = await fetchUserData(currentUser)
+        console.log({
+          connections: userConnections,
+          projectTags: userProjectTags,
+          connectedUsersTags: userConnectedTags,
+        })
+        const { data: allUsers } = await supabase.from('users2').select('*')
         console.log(allUsers)
         if (!allUsers) return
 
@@ -73,8 +112,19 @@ const FriendRecommendations: React.FC = () => {
 
         for (const user of allUsers) {
           if (currentUser !== user.auth_id && !userConnections.includes(user.auth_id) && user.auth_id) {
-            const { connections: otherConnections, interests: otherInterests } = await fetchUserData(user.auth_id)
-            const score = calculateRecommendationScore(userConnections, userInterests, otherConnections, otherInterests)
+            const {
+              connections: otherConnections,
+              projectTags: otherProjectTags,
+              connectedUsersTags: otherConnectedTags,
+            } = await fetchUserData(user.auth_id)
+            const score = calculateRecommendationScore(
+              userConnections,
+              userProjectTags,
+              userConnectedTags,
+              otherConnections,
+              otherProjectTags,
+              otherConnectedTags
+            )
             recommendations.push({ userId: user.auth_id, username: user.username, score })
           }
         }
@@ -97,10 +147,10 @@ const FriendRecommendations: React.FC = () => {
         console.log('Successfully connected to user:', friendId)
         const index = recommendations.findIndex((recommendation) => recommendation.userId === friendId)
         if (index !== -1) {
-          // Supprimer la recommandation de la liste
+          // Remove the recommendation from the list
           const updatedRecommendations = [...recommendations]
           updatedRecommendations.splice(index, 1)
-          setRecommendations(recommendations.filter((recommendation) => recommendation.userId !== friendId))
+          setRecommendations(updatedRecommendations)
         }
       }
     }
